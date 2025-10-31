5bcbbf659691c6840acf319943a0de7541337a56
precious654 <odukwechiemeka@gmail.com>
2025-10-31 02:24:06 +0100

    final commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,6 @@ export default function Home() {
   return (
     <main className="container">
       <Text />
-      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
-      <p className="mt-4 text-lg">
-        Get started by editing <code>app/page.tsx</code>
-      </p>
     </main>
   );
 }
--- components/Text.tsx
+++ components/Text.tsx
@@ -10,23 +10,105 @@ import {
   Center,
   useTexture,
 } from "@react-three/drei";
+import {
+  ChevronUp,
+  ChevronDown,
+  ChevronLeft,
+  ChevronRight,
+  Infinity,
+} from "lucide-react";
+import gsap from "gsap";
 
 export default function Text() {
+  const textRef = React.useRef<THREE.Mesh>(null);
+  const [rotate, setRotate] = React.useState(false);
+
+  const moveUp = () => {
+    if (!textRef.current) return;
+
+    const text = textRef.current;
+    gsap.to(text.position, {
+      y: `+=0.1`,
+      duration: 0.3,
+      ease: "power1.in",
+    });
+  };
+
+  const moveDown = () => {
+    if (!textRef.current) return;
+
+    const text = textRef.current;
+    gsap.to(text.position, {
+      y: `+=-0.1`,
+      duration: 0.3,
+      ease: "power1.in",
+    });
+  };
+
+  const moveRight = () => {
+    if (!textRef.current) return;
+
+    const text = textRef.current;
+    gsap.to(text.position, {
+      x: `+=0.1`,
+      duration: 0.3,
+      ease: "power1.in",
+    });
+  };
+
+  const moveLeft = () => {
+    if (!textRef.current) return;
+
+    const text = textRef.current;
+    gsap.to(text.position, {
+      x: `+=-0.1`,
+      duration: 0.3,
+      ease: "power1.in",
+    });
+  };
+
+  React.useEffect(() => {
+    if (!textRef.current) return;
+    const text = textRef.current;
+
+    const rotateText = () => {
+      text.rotation.y += 0.02; // smooth continuous rotation
+    };
+
+    if (rotate) {
+      gsap.ticker.add(rotateText);
+    } else {
+      gsap.ticker.remove(rotateText);
+    }
+
+    return () => {
+      gsap.ticker.remove(rotateText);
+    };
+  }, [rotate]);
+
   return (
     <div className="w-full h-screen">
       <Canvas shadows camera={{ position: [0, 0, 5], fov: 75 }}>
+        <color attach="background" args={["#e0e0e0"]} />
         <OrbitControls />
         <Environment
           files={"/assets/HDRI/pathway_morning_4k.hdr"}
-          environmentIntensity={0.5}
+          environmentIntensity={0.7}
         />
-        <directionalLight position={[0, 3, 3]} intensity={1} />
+        <directionalLight
+          position={[0, 3, 3]}
+          intensity={1}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+        />
         <Plane />
         <Center>
           <Text3D
             font="/assets/fonts/Avalors Personal Use Only_Regular.json"
             position={[0, 0, 14]}
             castShadow
+            receiveShadow
+            ref={textRef}
           >
             Hello, 3D!
             <meshStandardMaterial
@@ -37,6 +119,45 @@ export default function Text() {
           </Text3D>
         </Center>
       </Canvas>
+      <div className="mt-5">
+        <p className="text-2xl">Controls</p>
+        <div className="flex justify-between items-center mt-9">
+          <div className="flex flex-col gap-2 w-min items-center">
+            <button
+              className="border-2 p-2 rounded-full border-gray-300 hover:bg-gray-100 active:scale-95 transition"
+              onClick={moveUp}
+            >
+              <ChevronUp className="" />
+            </button>
+            <div className="flex gap-6 items-center">
+              <button
+                className="border-2 p-2 rounded-full border-gray-300 hover:bg-gray-100 active:scale-95 transition"
+                onClick={moveLeft}
+              >
+                <ChevronLeft className="" />
+              </button>
+              <button
+                className="border-2 p-2 rounded-full border-gray-300 hover:bg-gray-100 active:scale-95 transition"
+                onClick={moveRight}
+              >
+                <ChevronRight className="" />
+              </button>
+            </div>
+            <button
+              className="border-2 p-2 rounded-full border-gray-300 hover:bg-gray-100 active:scale-95 transition"
+              onClick={moveDown}
+            >
+              <ChevronDown className="" />
+            </button>
+          </div>
+          <button
+            className="border-2 p-2 rounded-full border-gray-300 hover:bg-gray-100 active:scale-95 transition"
+            onClick={() => setRotate(!rotate)}
+          >
+            <Infinity size={30} className="" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
@@ -61,6 +182,7 @@ const Plane = () => {
     roughnessMap: badlandRoughness,
     aoMap: badlandAO,
     roughness: 1,
+    color: "#777",
     side: THREE.DoubleSide,
   });
 
